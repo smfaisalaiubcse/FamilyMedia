16ff93dde06f9f732328e3e1e3b93a2a8fb8abc2
S M Faisal <smf48406@gmail.com>
2024-08-15 17:35:15 +0600

    u

--- FamilyMedia/Controllers/ChatController.cs
+++ FamilyMedia/Controllers/ChatController.cs
@@ -24,56 +24,6 @@ namespace FamilyMedia.Controllers
 
             return View(Convert(chat.ToList()));
         }
-        //[HttpGet]
-        //public ActionResult Index(int Id)
-        //{
-        //    int UserId = (int)Session["UserId"];
-        //    var chat = from c in db.Chats
-        //               where c.ReceiverId == UserId
-        //               select c;
-
-        //    return View(Convert(chat.ToList()));
-        //}
-        [HttpGet]
-        public ActionResult Create(int Id)
-        {
-            var receiver = (from r in db.Users
-                            where r.Id == Id
-                            select r).SingleOrDefault();
-            //return View(ProfileController.Convert(receiver));
-            return View();
-        }
-        [HttpPost]
-        public ActionResult Create(ChatDTO chat, int Id)
-        {
-            var receiver = (from r in db.Users
-                           where r.Id == Id
-                           select r).SingleOrDefault();
-            if (receiver == null) return RedirectToAction("ProfileNotFound");
-            if (ModelState.IsValid)
-            {
-                Chat newChat = new Chat
-                {
-                    Id = chat.Id,
-                    SenderId = (int)Session["UserId"],
-                    ReceiverId= Id,
-                    ChatData = chat.ChatData,
-                };
-                db.Chats.Add(newChat);
-                db.SaveChanges();
-                TempData["Msg"] = "Chat started Successful";
-                return RedirectToAction("Index");
-            }
-            else
-            {
-                TempData["Msg"] = "Validation Error: " + string.Join(", ", ModelState.Values
-                                                .SelectMany(v => v.Errors)
-                                                .Select(e => e.ErrorMessage));
-            }
-            return RedirectToAction("Index");
-            //return View(chat);
-            //return View(ProfileController.Convert(Profile));
-        }
         public static ChatDTO Convert(Chat chat)
         {
             return new ChatDTO()
